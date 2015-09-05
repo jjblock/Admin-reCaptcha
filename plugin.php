062cce96cc521bce73c319592a8c05cd8a37da1c
Abdul Rauf <armujahid@live.com>
2015-09-05 13:49:49 +0500

    Version 1.0

--- plugin.php
+++ plugin.php
@@ -1,25 +1,82 @@
 <?php
 /*
-Plugin Name: Your Plugin Name
-Plugin URI: http://github.com/your_name/your_plugin
-Description: One line description of your plugin
+Plugin Name: Admin reCaptcha
+Plugin URI: https://github.com/armujahid/Admin-reCaptcha.git
+Description: This plugin enable reCapcha on Admin login screen
 Version: 1.0
-Author: Your Name
-Author URI: http://your-site-if-any/
+Author: Abdul Rauf
+Author URI: http://armujahid.me/
 */
 
-// No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-/*
+yourls_add_action( 'pre_login_username_password', 'abdulrauf_adminreCaptcha_validatereCaptcha' );
 
- Your code goes here.
- 
- Suggested read:
- https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
- https://github.com/YOURLS/YOURLS/wiki#for-developpers
- https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
- 
- Have fun!
- 
-*/
+function abdulrauf_adminreCaptcha_validatereCaptcha()
+{
+	include('captcha.php'); 
+	if ($resp != null && $resp->success) 
+	{ 
+		//reCaptcha validated
+		return true;
+	}
+	else
+	{
+		yourls_do_action( 'login_failed' );
+		yourls_login_screen( $error_msg = 'reCaptcha validation failed' );
+		die();
+		return false;
+	}
+}
+
+// Register your plugin admin page
+yourls_add_action( 'plugins_loaded', 'abdulrauf_adminreCaptcha_init' );
+function abdulrauf_adminreCaptcha_init() {
+    yourls_register_plugin_page( 'adminreCaptcha', 'Admin reCaptcha Settings', 'adminreCaptcha_config_page' );
+}
+
+// The function that will draw the admin page
+function adminreCaptcha_config_page() {
+    	 if( isset( $_POST['abdulrauf_adminreCaptcha_public_key'] ) ) {
+	        yourls_verify_nonce( 'abdulrauf_adminreCaptcha_nonce' );
+	        abdulrauf_adminreCaptcha_save_admin();
+	    }
+    
+    $nonce = yourls_create_nonce( 'abdulrauf_adminreCaptcha_nonce' );
+    $pubkey = yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key', "" );
+    $privkey = yourls_get_option( 'abdulrauf_adminreCaptcha_priv_key', "" );
+    echo '<h2>Admin reCaptcha plugin settings</h2>';
+    echo '<form method="post">';
+    echo '<input type="hidden" name="nonce" value="' . $nonce . '" />';
+    echo '<p><label for="abdulrauf_adminreCaptcha_public_key">reCaptcha site key: </label>';
+    echo '<input type="text" id="abdulrauf_adminreCaptcha_public_key" name="abdulrauf_adminreCaptcha_public_key" value="' . $pubkey . '"></p>';  
+    echo '<p><label for="abdulrauf_adminreCaptcha_private_key">reCaptcha secret key: </label>';
+    echo '<input type="text" id="abdulrauf_adminreCaptcha_private_key" name="abdulrauf_adminreCaptcha_private_key" value="' . $privkey . '"></p>';
+    echo '<input type="submit" value="Save"/>';
+    echo '</form>';
+
+}
+
+function abdulrauf_adminreCaptcha_save_admin()
+{
+	$pubkey = $_POST['abdulrauf_adminreCaptcha_public_key'];
+	$privkey = $_POST['abdulrauf_adminreCaptcha_private_key'];
+	 if ( yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key' ) !== false ) {
+        yourls_update_option( 'abdulrauf_adminreCaptcha_pub_key', $pubkey );
+    } else {
+        yourls_add_option( 'abdulrauf_adminreCaptcha_pub_key', $pubkey );
+    }
+	 if ( yourls_get_option( 'abdulrauf_adminreCaptcha_priv_key' ) !== false ) {
+        yourls_update_option( 'abdulrauf_adminreCaptcha_priv_key', $privkey );
+    } else {
+        yourls_add_option( 'abdulrauf_adminreCaptcha_priv_key', $privkey );
+    }
+    echo "Saved";
+}
+yourls_add_action( 'plugins_loaded', 'abdulrauf_adminreCaptcha_plugin_init' );
+
+
+
+
+
+?>
