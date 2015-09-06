d1cb9588bb486f4c4781777328be00042cd2ea0a
Abdul Rauf <armujahid@live.com>
2015-09-06 20:52:50 +0500

    Version 1.1 Removal of core file hacks

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Admin reCaptcha
 Plugin URI: https://github.com/armujahid/Admin-reCaptcha.git
 Description: This plugin enable reCapcha on Admin login screen
-Version: 1.0
+Version: 1.1
 Author: Abdul Rauf
 Author URI: http://armujahid.me/
 */
@@ -63,21 +63,45 @@ function abdulrauf_adminreCaptcha_save_admin()
 {
 	$pubkey = $_POST['abdulrauf_adminreCaptcha_public_key'];
 	$privkey = $_POST['abdulrauf_adminreCaptcha_private_key'];
-	 if ( yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key' ) !== false ) {
+	if ( yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key' ) !== false ) {
         yourls_update_option( 'abdulrauf_adminreCaptcha_pub_key', $pubkey );
-    } else {
+    } 
+	else {
         yourls_add_option( 'abdulrauf_adminreCaptcha_pub_key', $pubkey );
     }
-	 if ( yourls_get_option( 'abdulrauf_adminreCaptcha_priv_key' ) !== false ) {
+	if ( yourls_get_option( 'abdulrauf_adminreCaptcha_priv_key' ) !== false ) {
         yourls_update_option( 'abdulrauf_adminreCaptcha_priv_key', $privkey );
-    } else {
+    } 
+	else {
         yourls_add_option( 'abdulrauf_adminreCaptcha_priv_key', $privkey );
     }
     echo "Saved";
 }
 
-
-
-
-
+// Add the JavaScript for reCaptcha widget
+yourls_add_action( 'html_head', 'abdulrauf_adminreCaptcha_addjs' );
+function abdulrauf_adminreCaptcha_addjs() {
+	$siteKey = yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key' );
+	?>
+	<script type="text/javascript">
+	//JQuery function to add div for reCaptcha widget and loadCaptcha after loading document
+	$(document).ready(function() {
+		var logindiv = document.getElementById('login');
+		if (logindiv != null) { //check if we are no login screen
+			var form = logindiv.innerHTML;
+			var index = form.indexOf('<p style="text-align: right;">');
+			document.getElementById('login').innerHTML = form.slice(0, index) + '<div id="captcha_container"></div>' + form.slice(index);	    
+			//loadCaptcha();
+		}
+    });
+	// JavaScript function to explicitly render the reCAPTCHA widget
+	var loadCaptcha = function() {
+	  captchaContainer = grecaptcha.render('captcha_container', {
+		'sitekey' : '<?php echo $siteKey?>'
+	  });
+	};
+	</script>
+	<script src="https://www.google.com/recaptcha/api.js?onload=loadCaptcha&render=explicit" async defer></script>
+	<?php
+}
 ?>
